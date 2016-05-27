24a3c51af523d5ded57b61adf9a2b2e9517836ad
Ryan Connolly <rynam0@gmail.com>
2016-05-27 13:37:03 -0400

    Try to account for the unhappy paths when authenticating and geocoding

--- lib/arcgis_geocode.ex
+++ lib/arcgis_geocode.ex
@@ -9,25 +9,36 @@ defmodule ArcgisGeocode do
   Geocodes the given address and returns a map.
   """
   def geocode(address) when is_binary(address) do
-    get_url(address)
-    |> HTTPoison.get
-    |> parse_geocode_response
-    |> extract_geocoded_address
+    case authenticate do
+      {:error, response} -> response
+      {:ok, response} ->
+        get_url(address, response["access_token"])
+        |> HTTPoison.get
+        |> parse_geocode_response
+        |> extract_geocoded_address
+    end
   end
 
-  defp parse_geocode_response({:ok, %{body: body}}), do: Poison.Parser.parse!(body)
+
+  defp parse_geocode_response({:ok, %{body: body}}), do: parse_response(body)
+  defp parse_geocode_response({:error, %{reason: reason}}), do: {:error, reason}
+  defp parse_geocode_response(%{"error" => _} = error), do: {:error, error}
+
+
+  defp extract_geocoded_address({:error, reason}), do: {:error, %{"error" => "API #{reason}"}}
 
   defp extract_geocoded_address(%{"locations" => [h|_]}) do
     feature = h["feature"]
     attributes = feature["attributes"]
-    %{lat: feature["geometry"]["x"],
-      lon: feature["geometry"]["y"],
-      street_number: attributes["AddNum"],
-      street: "#{attributes["StName"]} #{attributes["StType"]}",
-      city: attributes["City"],
-      state: attributes["Region"],
-      zip_code: attributes["Postal"],
-      formatted: h["name"]}
+    {:ok,
+      %{lat: feature["geometry"]["x"],
+        lon: feature["geometry"]["y"],
+        street_number: attributes["AddNum"],
+        street: "#{attributes["StName"]} #{attributes["StType"]}",
+        city: attributes["City"],
+        state: attributes["Region"],
+        zip_code: attributes["Postal"],
+        formatted: h["name"]}}
   end
 
   @doc """
@@ -35,15 +46,27 @@ defmodule ArcgisGeocode do
   """
   def authenticate do
     body = {:form, [{:client_id, @client_id}, {:client_secret, @client_secret}, {:grant_type, @grant_type}]}
-    HTTPoison.post(@auth_url, body)
-    |> extract_access_token
+    case HTTPoison.post(@auth_url, body) do
+      {:error, response} -> {:error, %{"error" => %{"reason" => response.reason}}}
+      {:ok, response} ->
+        parse_response(response.body)
+        |> process_authentication_response
+    end
   end
 
-  defp extract_access_token({:ok, %{body: body}}), do: Poison.Parser.parse!(body)["access_token"]
+  defp process_authentication_response(%{"error" => _} = error), do: {:error, error}
+  defp process_authentication_response(%{"access_token" => access_token, "expires_in" => expires_in}) do
+    {:ok, %{"access_token" => access_token, "expires_in" => expires_in}}
+  end
 
-  def get_url(address) do
+
+  defp parse_response(body) when is_binary(body) do
+    Poison.Parser.parse!(body)
+  end
+
+
+  def get_url(address, token) when is_binary(address) and is_binary(token) do
     address = URI.encode(address)
-    token = authenticate()
     "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?outFields=AddNum,StName,StType,City,Region,Postal&forStorage=%s&f=json&text=#{address}&token=#{token}"
   end
 
--- test/arcgis_geocode_test.exs
+++ test/arcgis_geocode_test.exs
@@ -3,15 +3,15 @@ defmodule ArcgisGeocodeTest do
   doctest ArcgisGeocode
 
   test "authenticate returns an access token" do
-    token = ArcgisGeocode.authenticate
-    assert token
+    result = ArcgisGeocode.authenticate
+    assert {:ok, _} = result
   end
 
   test "geocodes a good address" do
     geocoded = ArcgisGeocode.geocode("463 Mountain View Dr Colchester VT 05446")
-    assert geocoded == %{city: "Colchester", formatted: "463 Mountain View Dr, Colchester, Vermont, 05446",
+    assert geocoded == {:ok, %{city: "Colchester", formatted: "463 Mountain View Dr, Colchester, Vermont, 05446",
                          lat: -73.18369670074134, lon: 44.51295979206185, state: "Vermont", street: "Mountain View Dr",
-                         street_number: "463", zip_code: "05446"}
+                         street_number: "463", zip_code: "05446"}}
   end
 
 end
